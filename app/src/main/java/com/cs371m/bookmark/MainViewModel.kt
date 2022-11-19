d45deeddc4721c09adb3ee0ea1cd452bd2a78457
junfeng-utexas <junfeng@utexas.edu>
2022-11-20 00:14:38 +0800

    conflit

--- app/src/main/java/com/cs371m/bookmark/MainViewModel.kt
+++ app/src/main/java/com/cs371m/bookmark/MainViewModel.kt
@@ -56,7 +56,7 @@ class MainViewModel : ViewModel(){
             }
 
             Log.d("netRefresh", coverImageUrl(isbn, "S"))
-
+            val i = 3
             Log.d("netRefresh", repo.searchBookByTitle(searchTitle).toString())
         }
     }
