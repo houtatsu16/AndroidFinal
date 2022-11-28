e65098da938977aa6a6a9cb3f4e88bb9ccfca3f5
houtatsu16 <houtatsu16@yahoo.co.jp>
2022-11-28 15:03:25 +0900

    fixed bugs

--- app/src/main/java/com/cs371m/bookmark/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/cs371m/bookmark/ui/setting/SettingsFragment.kt
@@ -1,16 +1,13 @@
-package com.cs371m.bookmark.ui
+package com.cs371m.bookmark.ui.setting
 
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
-import com.cs371m.bookmark.MainActivity2
 import com.cs371m.bookmark.MainViewModel
 import com.cs371m.bookmark.databinding.FragmentSettingsBinding
 
